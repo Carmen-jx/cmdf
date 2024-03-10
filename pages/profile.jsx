2daf6d6bb212de5d90216c1b7ad91f2fd10e2bf2
Carmen Lam <148829232+Carmen-jx@users.noreply.github.com>
2024-03-10 14:51:54 -0400

    Update profile.jsx

---
+++ pages/profile.jsx
@@ -0,0 +1,14 @@
+import { Card, Form, Button, Alert } from "react-bootstrap";
+import { useState } from 'react';
+import { registerUser } from "@/lib/authenticate";
+import { useRouter } from 'next/router';
+
+
+export default function Profile(props){
+
+  return (
+    <>
+     <p>tbh</p>
+    </>
+  );
+}
